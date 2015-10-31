6b8c4b99489fe15a590a7572915e03855eaa354d
Maxime Gaudron <maxxg57@gmail.com>
2015-10-31 12:52:57 +0100

    custoer_in_range

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 var btnPushedArray []btnPushed
+var customerInRangeArray []customerInRange
 
 type btnPushed struct {
 	ID   string
@@ -19,9 +20,17 @@ type btnPushed struct {
 	Time string
 }
 
+type customerInRange struct {
+	CustomerID string
+	ButtonID   string
+	Date       string
+	Time       string
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/btn_pushed", buttonPushed)
+	router.HandleFunc("/btn_pushed", buttonPushedEvent)
+	router.HandleFunc("/customer_in_range", customerInRangeEvent)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -35,18 +44,36 @@ func stringsToSet(strings []string) mapset.Set {
 	return set
 }
 
-func buttonPushed(w http.ResponseWriter, r *http.Request) {
+func buttonPushedEvent(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 
 	decoder := json.NewDecoder(r.Body)
-	var t btnPushed
-	err := decoder.Decode(&t)
+	var event btnPushed
+	err := decoder.Decode(&event)
 	if err != nil {
 		panic(err)
 	}
 
-	btnPushedArray = append(btnPushedArray, t)
-	fmt.Printf("A button was pushed: %+v\n", i)
+	btnPushedArray = append(btnPushedArray, event)
+	fmt.Printf("Button %s was pushed\n", event.ID)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"success\": true}"))
+	log.Printf("%s\t%s\t%s", time.Since(start), r.Method, r.RequestURI)
+}
+
+func customerInRangeEvent(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	decoder := json.NewDecoder(r.Body)
+	var event customerInRange
+	err := decoder.Decode(&event)
+	if err != nil {
+		panic(err)
+	}
+
+	customerInRangeArray = append(customerInRangeArray, event)
+	fmt.Printf("Customer %s is in range of button %s\n", event.CustomerID, event.ButtonID)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("{\"success\": true}"))
